6a69370bfaec05b82b9a5923b1025a0a4ac7fbcc
Shu-Ting Huang <sthuang890702@gmail.com>
2021-12-20 10:20:25 -0500

    Create dto

---
+++ src/main/java/com/sg/blog/dto/PostTag.java
@@ -0,0 +1,22 @@
+package com.sg.blog.dto;
+
+public class PostTag {
+    private int postId;
+    private int tagId;
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(int tagId) {
+        this.tagId = tagId;
+    }
+}
