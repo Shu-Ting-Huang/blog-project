b9c6091ff09a8e35f72cdf547fa331075fa62512
Shu-Ting Huang <sthuang890702@gmail.com>
2021-12-20 14:38:05 -0500

    Create the first dao implementation

---
+++ src/main/java/com/sg/blog/dao/PostDaoImpl.java
@@ -0,0 +1,78 @@
+package com.sg.blog.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import com.sg.blog.dto.Post;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PostDaoImpl implements PostDao {
+
+    private static DataSource getDataSource() throws SQLException {
+        MysqlDataSource ds = new MysqlDataSource();
+        ds.setServerName("localhost");
+        ds.setDatabaseName("blog");
+        ds.setUser("root");
+        ds.setPassword("123");
+        ds.setUseSSL(false);
+        ds.setAllowPublicKeyRetrieval(true);
+        return ds;
+    }
+
+
+    @Override
+    public void addPost(Post post) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Post getPostById(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Post> getAllPosts() {
+        try {
+            List<Post> result = new ArrayList<>();
+            DataSource ds = getDataSource();
+            try (Connection conn = ds.getConnection()) {
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM posts");
+                while (rs.next()) {
+                    Post tempPost = new Post();
+                    tempPost.setId(rs.getInt("postId"));
+                    tempPost.setTitle(rs.getString("title"));
+                    tempPost.setContent(rs.getString("content"));
+                }
+            }
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void editPostById(int id, Post post) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void deletePostById(int id) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
