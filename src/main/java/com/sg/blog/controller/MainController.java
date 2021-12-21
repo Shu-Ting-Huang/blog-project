94234453dcdf56a96b99cf29ac9f5ce112d22071
Shu-Ting Huang <sthuang890702@gmail.com>
2021-12-21 10:43:10 -0500

    Reverse posts order

--- src/main/java/com/sg/blog/controller/MainController.java
+++ src/main/java/com/sg/blog/controller/MainController.java
@@ -1,11 +1,15 @@
 package com.sg.blog.controller;
 
+import java.util.Collections;
+import java.util.List;
+
 import com.sg.blog.dao.PostDao;
 import com.sg.blog.dao.PostDaoImpl;
 import com.sg.blog.dao.PostTagDao;
 import com.sg.blog.dao.PostTagDaoImpl;
 import com.sg.blog.dao.TagDao;
 import com.sg.blog.dao.TagDaoImpl;
+import com.sg.blog.dto.Post;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,16 +22,18 @@ public class MainController {
     
     @GetMapping("home")
     public String home(Model model, @RequestParam(name = "tag", required = false) String tagId) {
+        List<Post> postList;
         if (tagId == null) {
-            PostDao dao = new PostDaoImpl();
-            model.addAttribute("posts", dao.getAllPosts());
-            return "home";
+            PostDao postDao = new PostDaoImpl();
+            postList = postDao.getAllPosts();
         } else {
             PostTagDao postTagDao = new PostTagDaoImpl();
             TagDao tagDao = new TagDaoImpl();
-            model.addAttribute("posts", postTagDao.getPostsByTag(tagDao.getTagById(Integer.parseInt(tagId))));
-            return "home";
+            postList = postTagDao.getPostsByTag(tagDao.getTagById(Integer.parseInt(tagId)));
         }
+        Collections.reverse(postList);
+        model.addAttribute("posts", postList);
+        return "home";
     }
 
     @GetMapping("post/{postId}")
