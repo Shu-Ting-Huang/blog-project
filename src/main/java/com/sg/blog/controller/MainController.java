2db54e3810c3ce77dde1f7bca7510e553baa1de6
Shu-Ting Huang <sthuang890702@gmail.com>
2021-12-21 02:48:28 -0500

    Create clickable tags

--- src/main/java/com/sg/blog/controller/MainController.java
+++ src/main/java/com/sg/blog/controller/MainController.java
@@ -1,14 +1,16 @@
 package com.sg.blog.controller;
 
-import java.util.List;
-
 import com.sg.blog.dao.PostDao;
 import com.sg.blog.dao.PostDaoImpl;
-import com.sg.blog.dto.Post;
+import com.sg.blog.dao.PostTagDao;
+import com.sg.blog.dao.PostTagDaoImpl;
+import com.sg.blog.dao.TagDao;
+import com.sg.blog.dao.TagDaoImpl;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
@@ -16,8 +18,15 @@ public class MainController {
     @GetMapping("home")
     public String home(Model model) {
         PostDao dao = new PostDaoImpl();
-		List<Post> x = dao.getAllPosts();
-        model.addAttribute("posts", x);
+        model.addAttribute("posts", dao.getAllPosts());
+        return "home";
+    }
+
+    @GetMapping("tag/{tagId}")
+    public String tag(Model model, @PathVariable("tagId") String tagId) {
+        PostTagDao postTagDao = new PostTagDaoImpl();
+        TagDao tagDao = new TagDaoImpl();
+        model.addAttribute("posts", postTagDao.getPostsByTag(tagDao.getTagById(Integer.parseInt(tagId))));
         return "home";
     }
 }
