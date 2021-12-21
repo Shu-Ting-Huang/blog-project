892964b1761909c994dfe7545ea506779a7d5c5d
Shu-Ting Huang <sthuang890702@gmail.com>
2021-12-21 12:40:24 -0500

    Create the add post feature on web page

--- src/main/java/com/sg/blog/controller/MainController.java
+++ src/main/java/com/sg/blog/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.sg.blog.controller;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,20 +16,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+
+    PostDao postDao = new PostDaoImpl();
+    PostTagDao postTagDao = new PostTagDaoImpl();
+    TagDao tagDao = new TagDaoImpl();
     
     @GetMapping("home")
     public String home(Model model, @RequestParam(name = "tag", required = false) String tagId) {
         List<Post> postList;
         if (tagId == null) {
-            PostDao postDao = new PostDaoImpl();
             postList = postDao.getAllPosts();
         } else {
-            PostTagDao postTagDao = new PostTagDaoImpl();
-            TagDao tagDao = new TagDaoImpl();
             postList = postTagDao.getPostsByTag(tagDao.getTagById(Integer.parseInt(tagId)));
         }
         Collections.reverse(postList);
@@ -38,7 +41,6 @@ public class MainController {
 
     @GetMapping("post/{postId}")
     public String post(Model model, @PathVariable("postId") String postId) {
-        PostDao postDao = new PostDaoImpl();
         model.addAttribute("post", postDao.getPostById(Integer.parseInt(postId)));
         return "post";
     }
@@ -46,6 +48,13 @@ public class MainController {
     @GetMapping("admin")
     public String admin() {
         return "admin";
+    }
+
+    @PostMapping("admin/newpost")
+    public String newPost(@RequestParam("title") String title, @RequestParam("content") String content) {
+        List<String> tags= new ArrayList<>();
+        postDao.addPost(title, content, tags);
+        return "redirect:/home";
     }
 
 }
