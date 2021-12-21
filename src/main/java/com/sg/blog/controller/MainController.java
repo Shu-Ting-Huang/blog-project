a6ae1ec137a88b68ab840012c4e6c60df342e9ed
Shu-Ting Huang <sthuang890702@gmail.com>
2021-12-21 02:58:11 -0500

    Use query string parameters to filter tags

--- src/main/java/com/sg/blog/controller/MainController.java
+++ src/main/java/com/sg/blog/controller/MainController.java
@@ -10,23 +10,22 @@ import com.sg.blog.dao.TagDaoImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     
     @GetMapping("home")
-    public String home(Model model) {
-        PostDao dao = new PostDaoImpl();
-        model.addAttribute("posts", dao.getAllPosts());
-        return "home";
-    }
-
-    @GetMapping("tag/{tagId}")
-    public String tag(Model model, @PathVariable("tagId") String tagId) {
-        PostTagDao postTagDao = new PostTagDaoImpl();
-        TagDao tagDao = new TagDaoImpl();
-        model.addAttribute("posts", postTagDao.getPostsByTag(tagDao.getTagById(Integer.parseInt(tagId))));
-        return "home";
+    public String home(Model model, @RequestParam(name = "tag", required = false) String tagId) {
+        if (tagId == null) {
+            PostDao dao = new PostDaoImpl();
+            model.addAttribute("posts", dao.getAllPosts());
+            return "home";
+        } else {
+            PostTagDao postTagDao = new PostTagDaoImpl();
+            TagDao tagDao = new TagDaoImpl();
+            model.addAttribute("posts", postTagDao.getPostsByTag(tagDao.getTagById(Integer.parseInt(tagId))));
+            return "home";
+        }
     }
 }
